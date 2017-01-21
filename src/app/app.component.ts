118060f5f2318bc4e5f6fab78a5326e02da9bc64
unravel-the-sky <sekdemir@gmail.com>
2017-01-21 18:27:57 +0100

    fetching data works, with CORS! wee! now next step is to display them as table, then implement the sorting. but now, wardruna!;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ import "rxjs/Rx";
 export class AppComponent {
   constructor(private http: Http){}
 
-  datasetUrl = "http://data.ssb.no/api/v0/dataset/1088.json?lang=en";
+  datasetUrl = 'http://data.ssb.no/api/v0/dataset/1088.json';
+  // datasetUrl = 'https://coconut-cloud-service-dot-sntc-hackathon-vii.appspot.com/api/subscriptions';
   datasetInfo = [];
 
   // alright, let's begin. it's gonna be tremendous. 
@@ -23,7 +24,6 @@ export class AppComponent {
   }
 
   getJsonFromWebWithAngular2(){
-
     console.log("reading data began");
 
     this.http.get(this.datasetUrl)
@@ -32,18 +32,70 @@ export class AppComponent {
                         () => console.log("dataset is read"));
   }
 
+  // Create the XHR object.
+  createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+      // XHR for Chrome/Firefox/Opera/Safari.
+      xhr.open(method, url, true);
+    // } else if (typeof XDomainRequest != "undefined") {
+    //   // XDomainRequest for IE. --> though this is obselete and f.i.e.
+    //   xhr = new XDomainRequest();
+    //   xhr.open(method, url);
+    } else {
+      // CORS not supported.
+      xhr = null;
+    }
+    return xhr;
+  }
+  
+  // helper method
+  getTitle(text){
+    return text.match('<title>(.*)?</title>')[1];
+  }
+
+  makeCorsRequest(){
+    let xhr = this.createCORSRequest('GET', this.datasetUrl);
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
+
+    console.log("CORS is successfully done, data is coming!!");
+
+    xhr.onload = () => {
+      let text = xhr.responseText;
+      // let title = text.match('<title>(.*)?</title>')[1];
+      var jsonObject = JSON.parse(text);
+      this.datasetInfo = jsonObject;
+
+      console.log(this.datasetInfo);
+      // console.log("her er response fra CORS request " + text);
+    }
+
+    xhr.onerror = function() {
+      alert('poop happened');
+    }
+
+    xhr.send();
+
+  }
+
   getJsonFromWeb() {
     console.log("fetching data");
 
-    var xmlHttp = new XMLHttpRequest();
+    this.makeCorsRequest();
+
+    // var xmlHttp = new XMLHttpRequest();
     
-    xmlHttp.open( "GET", this.datasetUrl, false ); // false for synchronous request
-    //xmlHttp.setRequestHeader("Content-Type", "application/json");
+    // xmlHttp.open( "GET", this.datasetUrl, false ); // false for synchronous request
+    // xmlHttp.setRequestHeader("Content-Type", "application/json");
+    // xmlHttp.send( null );
 
-    var jsonObject = JSON.parse(xmlHttp.responseText);
-    this.datasetInfo = jsonObject;
+    // var jsonObject = JSON.parse(xmlHttp.responseText);
+    // this.datasetInfo = jsonObject;
 
-    console.log(this.datasetInfo);
+    // console.log(this.datasetInfo);
 
     console.log("fetching completed");
   }
